3fcdabf2afd9907a6f61c1f96e0eaadd5a8c2c5e
THAY1688 <koemthay.tha@student.passerellesnumeriques.com>
2025-06-03 15:22:08 +0700

    finished label class

--- src/project/Label.ts
+++ src/project/Label.ts
@@ -1,11 +1,35 @@
-export class Label {
+import { Task } from "../task_management/Task";
+
+
+export class Label{
   private labelId: number;
   private name: string;
 
-  constructor(labelId: number, name: string) {
+  constructor(
+    labelId: number,
+    name: string,
+  ) {
     this.labelId = labelId;
     this.name = name;
   }
 
-  getName(): string { return this.name; }
+  getLabelId(): number {
+    return this.labelId;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  setName(name: string): void {
+    this.name = name;
+  }
+
+  attachToTask(task: Task): void {
+    task.addLabel(this);
+  }
+
+  removeFromTask(task: Task): void {
+    const updatedLabels = task.getLabels().filter(l => l.getLabelId() !== this.labelId);
+  }
 }
